f2c3d47134a647d0c1109926d1b8a3a3880a432c
ruperteb <59008625+ruperteb@users.noreply.github.com>
2020-11-05 20:07:18 +0200

    heroku express7

--- src/index.js
+++ src/index.js
@@ -84,7 +84,14 @@ const corsOptions = {
 
 const app = express();
 /* app.use(cors()) */
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 
 
