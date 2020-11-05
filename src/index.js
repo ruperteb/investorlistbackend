c331a0fe25eaeaeb23aebfa77fce618edccc716b
ruperteb <ruperteb@gmail.com>
2020-11-05 11:38:07 +0200

    heroku5

--- src/index.js
+++ src/index.js
@@ -45,14 +45,14 @@ const opts = {
   }
 };
 
-server.express.use(function(req, res, next) {
+/* server.express.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
   next();
-});
+}); */
 
 
 const server = new GraphQLServer({
